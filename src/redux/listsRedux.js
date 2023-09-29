54854fd8e70bdfa04f5c49195f4641842d7ee672
Ola <cencora.aleksandra@gmail.com>
2023-09-29 14:11:25 +0200

    Change reducer structure

--- src/redux/listsRedux.js
+++ src/redux/listsRedux.js
@@ -1,6 +1,18 @@
+//selectors
+export const getAllLists = ({ lists }) => lists;
+export const getListById = ({ lists }, listId) =>
+  lists.find((list) => list.id === listId);
+
+//actions
+const createActionName = actionName => `app/lists/${actionName}`;
+const ADD_LIST = createActionName('ADD_LIST');
+
+//action creators
+export const addList = (payload) => ({ type: ADD_LIST, payload });
+
 const listsReducer = (statePart = [], action) => {
   switch (action.type) {
-    case "ADD_LIST":
+    case ADD_LIST:
       return [...statePart, { ...action.payload }];
     default:
       return statePart;
