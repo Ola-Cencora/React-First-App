a48fc6c4689216c282b2dba6f748d03a1ad28647
Ola <cencora.aleksandra@gmail.com>
2023-10-02 12:41:24 +0200

    Add removing cards feature

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,7 +1,7 @@
 import styles from "./Card.module.scss";
 import clsx from "clsx";
 import { useDispatch } from "react-redux";
-import { toggleCardFavorite } from "../../redux/cardsRedux";
+import { toggleCardFavorite, removeCard } from "../../redux/cardsRedux";
 
 const Card = ({ id, title, isFavorite }) => {
   const dispatch = useDispatch();
@@ -11,15 +11,28 @@ const Card = ({ id, title, isFavorite }) => {
     dispatch(toggleCardFavorite(id));
   };
 
+  const remove = (e) => {
+    e.preventDefault();
+    dispatch(removeCard(id));
+  };
+
   return (
     <li className={styles.card}>
       {title}
-      <button
-        onClick={toggleFavorite}
-        className={clsx(styles.button, isFavorite === true && styles.favorite)}
-      >
-        <span className="fa fa-star-o"></span>
-      </button>
+      <div>
+        <button
+          onClick={toggleFavorite}
+          className={clsx(
+            styles.button,
+            isFavorite === true && styles.favorite
+          )}
+        >
+          <span className="fa fa-star-o"></span>
+        </button>
+        <button onClick={remove} className={clsx(styles.button, styles.remove)}>
+          <span className="fa fa-trash"></span>
+        </button>
+      </div>
     </li>
   );
 };
